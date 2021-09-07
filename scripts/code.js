cc79d627e7094a8af2a94083d39d09657946c86f
rayrixo <doomknight150@gmail.com>
2021-09-07 21:17:01 +0800

    updated2

--- scripts/code.js
+++ scripts/code.js
@@ -1,7 +1,7 @@
 window.onload = () => {
-    let places  = staticLoadPlaces();
+    let places = staticLoadPlaces();
     renderPlaces(places);
-}
+};
 
 function staticLoadPlaces() {
     return [
@@ -40,24 +40,24 @@ function staticLoadPlaces() {
     ];
 }
 
-function renderPlaces(places){
-    let scene  = document.querySelector('a-scene');
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
 
-    places.forEach((place)=>{
-        let latitude  = place.location.lat
-        let longitude = place.location.lng
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', 'assets/asset.gltf');
-        model.setAttribute('look-at', '[gps-camera]');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.05 0.05 0.05');
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', 'assets/asset.gltf');
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.5 0.5 0.5');
 
-        model.addEventListener('loaded',() => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
 
-        scene.appendChild(model);
-    });
+       scene.appendChild(model);
+   });
 }
