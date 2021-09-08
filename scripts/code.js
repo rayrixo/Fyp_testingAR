16f3732e19cf173f13b136d24cb4c53f8972af4a
rayrixo <doomknight150@gmail.com>
2021-09-08 11:20:20 +0800

    enabled alert

--- scripts/code.js
+++ scripts/code.js
@@ -10,43 +10,37 @@ function staticLoadPlaces() {
            location: {
                lat: 1.378043,
                lng: 103.850071,
-           },
-
-           name:'Testing',
-           location:{
-               lat: 1.379198,
-               lng: 103.849562,
            }
 
        },
 
-        // {
-        //     name: 'YCK',
-        //     location: {
-        //         lat: 1.381590,
-        //         lng: 103.844905,
-        //     }
+        {
+            name: 'YCK',
+            location: {
+                lat: 1.381590,
+                lng: 103.844905,
+            }
 
-        // },
+        },
         
-        // {
-        //     name: 'Block_A',
-        //     location: {
-        //         lat: 1.380099,
-        //         lng: 103.848593,
-        //     }
+        {
+            name: 'Block_A',
+            location: {
+                lat: 1.380099,
+                lng: 103.848593,
+            }
 
-        // },
+        },
 
 
-        // {
-        //     name: 'Block_L',
-        //     location: {
-        //         lat: 1.379198,
-        //         lng: 103.849562,
-        //     }
+        {
+            name: 'Block_L',
+            location: {
+                lat: 1.379198,
+                lng: 103.849562,
+            }
 
-        // },
+        },
        
    ];
 }
