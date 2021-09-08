f35c281410cc75b99510b420e85d4354b2c27f30
rayrixo <doomknight150@gmail.com>
2021-09-08 22:33:19 +0800

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -69,8 +69,7 @@ function renderPlaces(places) {
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         model.setAttribute('gltf-model', `${test}`);
         model.setAttribute('rotation', '0 180 0');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.5 0.5 0.5');
+        model.setAttribute('scale', '5 5 5');
 
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
