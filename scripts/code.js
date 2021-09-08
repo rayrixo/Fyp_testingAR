105792da36406f864748baa4c56a43eafd226fc2
rayrixo <doomknight150@gmail.com>
2021-09-08 11:07:04 +0800

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -11,7 +11,37 @@ function staticLoadPlaces() {
                lat: 1.378043,
                lng: 103.850071,
            }
+
        },
+
+        {
+            name: 'YCK',
+            location: {
+                lat: 1.381590,
+                lng: 103.844905,
+            }
+
+        },
+        
+        {
+            name: 'Block_A',
+            location: {
+                lat: 1.380099,
+                lng: 103.848593,
+            }
+
+        },
+
+
+        {
+            name: 'Block_L',
+            location: {
+                lat: 1.379198,
+                lng: 103.849562,
+            }
+
+        },
+       
    ];
 }
 
@@ -27,7 +57,7 @@ function renderPlaces(places) {
        model.setAttribute('gltf-model', 'assets/asset.gltf');
        model.setAttribute('look-at','[gps-camera]');
        model.setAttribute('scale', '5 5 5');
-       model.setAttribute('position','0 20 30');
+       model.setAttribute('position','0 20 100');
 
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
