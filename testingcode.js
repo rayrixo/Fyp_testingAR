5035cadb0d7e8d2e0ea383861c0981eb973e7453
rayrixo <doomknight150@gmail.com>
2021-09-09 12:34:12 +0800

    chaanged something quite a bit

--- testingcode.js
+++ testingcode.js
@@ -13,8 +13,7 @@ function staticLoadPlaces() {
             location: {
                 lat: 1.378043,
                 lng: 103.850071,
-            },
-            test:'assets/asset.gltf'
+            }
  
         },
  
@@ -23,8 +22,7 @@ function staticLoadPlaces() {
              location: {
                  lat: 1.381590,
                  lng: 103.844905,
-             },
-             test:'assets/asset.gltf'
+             }
  
          },
          
@@ -33,8 +31,7 @@ function staticLoadPlaces() {
              location: {
                  lat: 1.380099,
                  lng: 103.848593,
-             },
-             test:'assets/asset.gltf'
+             }
  
          },
  
@@ -44,8 +41,7 @@ function staticLoadPlaces() {
              location: {
                  lat: 1.379198,
                  lng: 103.849562,
-             },
-             test:'assets/asset.gltf'
+             }
  
          },
         
