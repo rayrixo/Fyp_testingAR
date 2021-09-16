164af6bc400f189591556f4e26f93945334da6e5
rayrixo <doomknight150@gmail.com>
2021-09-16 17:19:44 +0800

    Update code2.js

--- scripts/code2.js
+++ scripts/code2.js
@@ -29,9 +29,9 @@ const loadPlaces = function(coords) {
         },
     ];
 
-    if (method === 'api') {
-        return loadPlaceFromAPIs(coords);
-    }
+    // if (method === 'api') {
+    //     return loadPlaceFromAPIs(coords);
+    // }
 
     return Promise.resolve(PLACES);
 };
