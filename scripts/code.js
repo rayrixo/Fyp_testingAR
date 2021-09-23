a0565965a5822464b8766e8bbd21fc66fe6086ec
rayrixo <doomknight150@gmail.com>
2021-09-23 12:25:53 +0800

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -99,8 +99,7 @@ window.onload = () => {
                     icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
                     icon.setAttribute('name', place.name);
                     icon.setAttribute('src', './assets/map-marker.png');
-                     var distance = icon.getAttribute("distance");
-                     console.log(distance);
+                    icon.setAttribute('visible',false);
 
                     // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
                     icon.setAttribute('scale', '20, 20');
