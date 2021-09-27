90546afbf1324112a4f56da0b6885bc45492be3f
rayrixo <doomknight150@gmail.com>
2021-09-27 13:59:59 +0800

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -108,7 +108,7 @@ window.onload = () => {
                     icon.setAttribute('name', place.name);
                     icon.setAttribute('src', './assets/map-marker.png');
                     distancemsg = icon.getAttribute('distance');
-                    if(distancemsg<12){
+                    if(distancemsg<15){
                         icon.setAttribute('visible', false);
                     }else{
                          icon.setAttribute('visible',true);
