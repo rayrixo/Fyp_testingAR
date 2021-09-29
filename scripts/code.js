bee6a6aa702445b35b796a6ff25cd6c9e5464c5c
rayrixo <doomknight150@gmail.com>
2021-09-29 16:01:26 +0800

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -95,61 +95,33 @@ window.onload = () => {
     // first get current user location
     return navigator.geolocation.getCurrentPosition(function (position) {
 
-        // then use it to load from remote APIs some places nearby
+        // than use it to load from remote APIs some places nearby
         loadPlaces(position.coords)
             .then((places) => {
                 places.forEach((place) => {
                     const latitude = place.location.lat;
                     const longitude = place.location.lng;
 
-                    // add place icon
-                    const icon = document.createElement('a-link');
-                    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-                    icon.setAttribute('title', place.name);
-                    icon.setAttribute('href', 'google.com');
+                    // add place name
+                    const text = document.createElement('a-link');
+                    text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+                    text.setAttribute('title', place.name);
+                    text.setAttribute('href', 'http://www.example.com/');
+                    text.setAttribute('scale', '20 20 20');
 
+                    text.addEventListener('loaded', () => {
+                        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+                    });
 
-                    // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-                    icon.setAttribute('scale', '20, 20');
-
-                    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
-
-                    // const clickListener = function(ev) {
-                    //     ev.stopPropagation();
-                    //     ev.preventDefault();
-
-                    //     const name = ev.target.getAttribute('name');
-
-                    //     const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-                    //     if (el && el === ev.target) {
-                    //         const label = document.createElement('span');
-                    //         const container = document.createElement('div');
-                    //         container.setAttribute('id', 'place-label');
-                    //         label.innerText = name;
-                    //         container.appendChild(label);
-                    //         document.body.appendChild(container);
-
-                    //         setTimeout(() => {
-                    //             container.parentElement.removeChild(container);
-                    //         }, 2500);
-                    //     }
-                    // };
-
-                    // icon.addEventListener('click', clickListener);
-                    
-                    scene.appendChild(icon);
-                    
-                   
+                    scene.appendChild(text);
                 });
-
             })
     },
         (err) => console.error('Error in retrieving position', err),
         {
             enableHighAccuracy: true,
             maximumAge: 0,
-            timeout: 20000,
+            timeout: 27000,
         }
     );
 };
