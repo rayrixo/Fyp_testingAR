833ca4b78e02b0be5541a4b1717b803ad8d1bc18
rayrixo <doomknight150@gmail.com>
2021-09-30 17:05:28 +0800

    Update code.js

--- scripts/code.js
+++ scripts/code.js
@@ -106,9 +106,6 @@ window.onload = () => {
 
                     // add place icon
                     const icon = document.createElement('a-image');
-                    const link = document.createElement('a-link');
-                    link.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
-                    link.setAttribute('href','google.com');
                     icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
                     icon.setAttribute('name', place.name);
                     icon.setAttribute('src', './assets/map-marker.png');
@@ -119,30 +116,29 @@ window.onload = () => {
 
                     icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
-                    // const clickListener = function(ev) {
-                    //     ev.stopPropagation();
-                    //     ev.preventDefault();
+                    const clickListener = function(ev) {
+                        ev.stopPropagation();
+                        ev.preventDefault();
 
-                    //     const name = ev.target.getAttribute('name');
+                        const name = ev.target.getAttribute('name');
 
-                    //     const el = ev.detail.intersection && ev.detail.intersection.object.el;
+                        const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-                    //     if (el && el === ev.target) {
-                    //         const label = document.createElement('span');
-                    //         const container = document.createElement('div');
-                    //         container.setAttribute('id', 'place-label');
-                    //         label.innerText = name;
-                    //         container.appendChild(label);
-                    //         document.body.appendChild(container);
+                        if (el && el === ev.target) {
+                            const label = document.createElement('span');
+                            const container = document.createElement('div');
+                            container.setAttribute('id', 'place-label');
+                            label.innerText = name;
+                            container.appendChild(label);
+                            document.body.appendChild(container);
 
-                    //         setTimeout(() => {
-                    //             container.parentElement.removeChild(container);
-                    //         }, 2500);
-                    //     }
-                    // };
+                            setTimeout(() => {
+                                container.parentElement.removeChild(container);
+                            }, 2500);
+                        }
+                    };
 
-                    // icon.addEventListener('click', clickListener);
-                    scene.appendChild(link);
+                    icon.addEventListener('click', clickListener);
                     scene.appendChild(icon);
                 });
             })
